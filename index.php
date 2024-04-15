4f0fcff93e6bbbf694326a8df23f84a526093d50
alessandrodamato <alessandro042004@gmail.com>
2024-04-15 15:20:33 +0200

    [add] bonus

--- index.php
+++ index.php
@@ -40,6 +40,9 @@ $hotels = [
 
 ];
 
+  $voto = $_GET['voto'];
+  $parcheggio = $_GET['parcheggio'];
+
 ?>
 
 <!DOCTYPE html>
@@ -66,7 +69,7 @@ $hotels = [
 
             <div class="w-100 h-100 d-flex justify-content-end align-items-center">
               <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                <input class="form-check-input" type="checkbox" id="flexCheckDefault" name="parcheggio" <?php if ($parcheggio !== null) echo 'checked' ?>>
                 <label class="form-check-label" for="flexCheckDefault">
                   Parcheggio incluso
                 </label>
@@ -78,11 +81,12 @@ $hotels = [
           <div class="col-3">
 
               <select name="voto" id="voto" class="form-select">
-                <option value="1">Una stella o più</option>
-                <option value="2">Due stelle o più</option>
-                <option value="3">Tre stelle o più</option>
-                <option value="4">Quattro stelle o più</option>
-                <option value="5">Cinque stelle o più</option>
+                <option value="" <?php if ($voto === null || $voto === '') echo 'selected'?> >Seleziona voto...</option>
+                <option value="1" <?php if ($voto == 1) echo 'selected'?> >Una stella o più</option>
+                <option value="2" <?php if ($voto == 2) echo 'selected'?> >Due stelle o più</option>
+                <option value="3" <?php if ($voto == 3) echo 'selected'?> >Tre stelle o più</option>
+                <option value="4" <?php if ($voto == 4) echo 'selected'?> >Quattro stelle o più</option>
+                <option value="5" <?php if ($voto == 5) echo 'selected'?> >Cinque stelle o più</option>
               </select>
 
           </div>
@@ -108,23 +112,25 @@ $hotels = [
       <div class="row row-cols-3">
 
         <?php foreach ($hotels as $hotel) : ?>
-          <div class="col">
+          <?php if (($parcheggio !== null && $hotel['parking'] || $parcheggio === null) && $hotel['vote'] >= $voto || $voto === null ): ?>
+            <div class="col">
 
-            <div class="card mb-5">
-              <img src="https://picsum.photos/id/<?php echo rand(1, 500) ?>/300/300" class="card-img-top" alt="<?php echo $hotel['name'] ?>" class="img-fluid">
-              <div class="card-body">
-                <h5 class="card-title"><?php echo $hotel['name'] ?></h5>
-                <p class="card-text"><?php echo $hotel['description'] ?></p>
+              <div class="card mb-5">
+                <img src="https://picsum.photos/id/<?php echo rand(50, 70) ?>/300" class="card-img-top" alt="<?php echo $hotel['name'] ?>" class="img-fluid">
+                <div class="card-body">
+                  <h5 class="card-title"><?php echo $hotel['name'] ?></h5>
+                  <p class="card-text"><?php echo $hotel['description'] ?></p>
+                </div>
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item">Parcheggio: <?php echo $hotel['parking'] ? 'Sì' : 'No' ?></li>
+                  <li class="list-group-item">Voto: <?php echo $hotel['vote'] ?> &star;</li>
+                  <li class="list-group-item">Distanza dal centro: <?php echo $hotel['distance_to_center'] ?> Km</li>
+                </ul>
+
               </div>
-              <ul class="list-group list-group-flush">
-                <li class="list-group-item">Parcheggio: <?php echo $hotel['parking'] ? 'Sì' : 'No' ?></li>
-                <li class="list-group-item">Voto: <?php echo $hotel['vote'] ?></li>
-                <li class="list-group-item">Distanza dal centro: <?php echo $hotel['distance_to_center'] ?></li>
-              </ul>
 
             </div>
-
-          </div>
+          <?php endif ?>
         <?php endforeach ?>
 
       </div>
