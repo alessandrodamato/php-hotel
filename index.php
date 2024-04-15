82e0e653c5dc4ee95379e8a756eddb6c8f5d142e
alessandrodamato <alessandro042004@gmail.com>
2024-04-15 13:45:52 +0200

    [add] dynamic hotels in bootstrap cards

--- index.php
+++ index.php
@@ -1,66 +1,91 @@
 <?php
 
-  $hotels = [
+$hotels = [
 
-    [
-      'name' => 'Hotel Belvedere',
-      'description' => 'Hotel Belvedere Descrizione',
-      'parking' => true,
-      'vote' => 4,
-      'distance_to_center' => 10.4
-    ],
-    [
-      'name' => 'Hotel Futuro',
-      'description' => 'Hotel Futuro Descrizione',
-      'parking' => true,
-      'vote' => 2,
-      'distance_to_center' => 2
-    ],
-    [
-      'name' => 'Hotel Rivamare',
-      'description' => 'Hotel Rivamare Descrizione',
-      'parking' => false,
-      'vote' => 1,
-      'distance_to_center' => 1
-    ],
-    [
-      'name' => 'Hotel Bellavista',
-      'description' => 'Hotel Bellavista Descrizione',
-      'parking' => false,
-      'vote' => 5,
-      'distance_to_center' => 5.5
-    ],
-    [
-      'name' => 'Hotel Milano',
-      'description' => 'Hotel Milano Descrizione',
-      'parking' => true,
-      'vote' => 2,
-      'distance_to_center' => 50
-    ],
+  [
+    'name' => 'Hotel Belvedere',
+    'description' => 'Hotel Belvedere Descrizione',
+    'parking' => true,
+    'vote' => 4,
+    'distance_to_center' => 10.4
+  ],
+  [
+    'name' => 'Hotel Futuro',
+    'description' => 'Hotel Futuro Descrizione',
+    'parking' => true,
+    'vote' => 2,
+    'distance_to_center' => 2
+  ],
+  [
+    'name' => 'Hotel Rivamare',
+    'description' => 'Hotel Rivamare Descrizione',
+    'parking' => false,
+    'vote' => 1,
+    'distance_to_center' => 1
+  ],
+  [
+    'name' => 'Hotel Bellavista',
+    'description' => 'Hotel Bellavista Descrizione',
+    'parking' => false,
+    'vote' => 5,
+    'distance_to_center' => 5.5
+  ],
+  [
+    'name' => 'Hotel Milano',
+    'description' => 'Hotel Milano Descrizione',
+    'parking' => true,
+    'vote' => 2,
+    'distance_to_center' => 50
+  ],
 
-  ];
+];
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <title>PHP Hotel</title>
 </head>
+
 <body>
 
-  <?php foreach($hotels as $hotel): ?>
-  <ul>
-    <li><?php echo $hotel['name'] ?></li>
-    <li><?php echo $hotel['description'] ?></li>
-    <li><?php echo $hotel['parking'] ? 'SI' : 'NO' ?></li>
-    <li><?php echo $hotel['vote'] ?></li>
-    <li><?php echo $hotel['distance_to_center'] ?></li>
-  </ul>
-  <?php endforeach ?>
-  
+  <main class="mt-5">
+    
+    <div class="container">
+
+      <div class="row row-cols-3">
+
+        <?php foreach ($hotels as $hotel) : ?>
+          <div class="col">
+
+            <div class="card mb-5">
+              <img src="https://picsum.photos/id/<?php echo rand(1, 500) ?>/300/300" class="card-img-top" alt="<?php echo $hotel['name'] ?>" class="img-fluid">
+              <div class="card-body">
+                <h5 class="card-title"><?php echo $hotel['name'] ?></h5>
+                <p class="card-text"><?php echo $hotel['description'] ?></p>
+              </div>
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item">Parcheggio: <?php echo $hotel['parking'] ? 'Sì' : 'No' ?></li>
+                <li class="list-group-item">Voto: <?php echo $hotel['vote'] ?></li>
+                <li class="list-group-item">Distanza dal centro: <?php echo $hotel['distance_to_center'] ?></li>
+              </ul>
+
+            </div>
+
+          </div>
+        <?php endforeach ?>
+
+      </div>
+
+    </div>
+
+  </main>
+
 </body>
+
 </html>
